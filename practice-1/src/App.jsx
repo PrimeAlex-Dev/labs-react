bcae5a25abfb3d3aa802beab8177583bdf356676
Nguyen Huy <huynt.ce190721@gmail.com>
2026-09-14 07:42:31 +0700

    Add programming practices

---
+++ practice-1/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx"
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <p> Hello My name is Huy </p>
+            <Footer/>
+        </>
+    )
+}
+
+export default App
---
+++ practice-1/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p>copy right 2026</p>
+        </footer>
+    );
+}
+export default Footer;
